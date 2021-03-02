cb0f2349043a5aceed49ecf1d4d976481b11e095
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2021-03-02 22:59:32 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,28 @@
-print("hello guy")
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Title </title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="my.css" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </head>
+    <body>
+      <header>
+        <h1>
+          h1 in header
+        </h1>
+      </header>
+      <nav>
+      </nav>
+      <main>
+        <section>
+          <h1>
+            This is a h1 in an a section
+          </h1>
+          <p>
+            p in section
+          </p>
+        </section>
+      </main>
+    </body>
+</html>
